b65fa6e6298147332d90369cb3bf316c1a83144d
Flobul <flobul.jeedom@gmail.com>
2020-11-02 15:13:32 +0100

    Update globalcache.php

--- core/bridges/globalcache.php
+++ core/bridges/globalcache.php
@@ -23,7 +23,7 @@ class scan_ip_globalcache {
     * -> $return[idEquipement]["ip_v4"] = l'ip enregistré au format v4
     */
     public function getAllElements(){
-
+        $return = NULL;
         $eqLogics = eqLogic::byType(self::$plug); 
         
         foreach ($eqLogics as $eqLogic) {    
