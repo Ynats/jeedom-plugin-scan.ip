92fe6da14ade877c32c304dfc9ea375b4f80dfe3
Ynats <dev@ynats.me>
2020-11-16 19:31:44 +0100

    -

--- desktop/modal/yes_equipement.php
+++ desktop/modal/yes_equipement.php
@@ -112,7 +112,7 @@ require_once dirname(__FILE__) . "/../../../../plugins/scan_ip/core/class/scan_i
             </table>
         </div>
     </div>
-    <div id="div_alert_scan_ip" class="jqAlert alert-warning" style="display:none;"><span class="displayError">Pour le moment, vous n'avez enregistré aucune équipement MAC.</span></div>
+    <div id="div_alert_scan_ip" class="jqAlert alert-warning" style="display:none;"><span class="displayError">Pour le moment, vous n'avez pas enregistré d'équipement MAC.</span></div>
 </div>
   
 <?php include_file('desktop', 'lib/stupidtable.min', 'js', 'scan_ip'); ?>
