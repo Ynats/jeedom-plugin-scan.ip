32dff1ca77492e4d4c068416238d38ff9bb37c0a
Ynats <dev@ynats.me>
2020-11-18 12:12:02 +0100

    - Maj Bridge Zigbee

--- core/class/bridges/zigbee.php
+++ core/class/bridges/zigbee.php
@@ -10,7 +10,8 @@ class scan_ip_zigbee {
     * Nom de la variable ip à modifier
     */
     public static $plug = "zigbee";
-    public static $ip = "wifizigate";
+    public static $ip = "wifizigate_";
+    public static $nb = 2;
     
     /**
     * getAllElements sert à récupérer les infos des éléments liés au plugin
@@ -23,19 +24,24 @@ class scan_ip_zigbee {
     * -> $return[idEquipement]["ip_v4"] = l'ip enregistré au format v4
     */
     public function getAllElements(){
-
-        $return[self::$plug]["plugin"] = self::$plug;
-        $return[self::$plug]["plugin_print"] = self::$plug;
-        $return[self::$plug]["name"] = "ZiGate Wifi";
-        $return[self::$plug]["id"] = "config";
         
-        $value = config::byKey(self::$ip, self::$plug);
-        if(preg_match(scan_ip_tools::getRegex("ip_v4"), $value, $match)){
-            $return[self::$plug]["ip_v4"] = $match[0];
-            return $return;
-        } else {
-           return NULL; 
+        for($i = 1; $i <= self::$nb; $i++){
+            $return[self::$plug.$i]["plugin"] = self::$plug;
+            $return[self::$plug.$i]["plugin_print"] = self::$plug . ": Démon " . $i;
+            $return[self::$plug.$i]["name"] = "ZiGate Wifi ".$i;
+            $return[self::$plug.$i]["id"] = self::$ip.$i;
+        
+            $value = config::byKey(self::$ip.$i, self::$plug);       
+            if(!empty($value)){
+                if(preg_match(scan_ip_tools::getRegex("ip_v4"), $value, $match)){
+                    $return[self::$plug.$i]["ip_v4"] = $match[0];
+                } else {
+                   $return[self::$plug.$i]["ip_v4"] =  NULL; 
+                }
+            }
         }
+        
+        return $return;
         
     }
     
@@ -49,11 +55,11 @@ class scan_ip_zigbee {
     */
     public function majIpElement($_ip ,$_id){
         
-        $old = config::byKey(self::$ip, self::$plug);
+        $old = config::byKey($_id, self::$plug);
         preg_match(scan_ip_tools::getRegex("ip_v4"), $old, $match);
         if($match[0] != $_ip) { 
             $change_ip = preg_replace(scan_ip_tools::getRegex("ip_v4"), $_ip, $old);
-            config::save(self::$ip, $change_ip, self::$plug);
+            config::save($_id, $change_ip, self::$plug);
             // Retourne le deamon à lancer
             return self::$plug;
         }
