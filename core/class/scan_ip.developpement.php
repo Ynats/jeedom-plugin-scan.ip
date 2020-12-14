cdd2a4e8101ef6378e42406fbb76e4a74977935e
Ynats <dev@ynats.me>
2020-12-14 02:15:22 +0100

    Cache MAC id

--- core/class/scan_ip.developpement.php
+++ core/class/scan_ip.developpement.php
@@ -10,7 +10,7 @@ require_once __DIR__ . "/../../../../plugins/scan_ip/core/class/scan_ip.require_
 
 class scan_ip_dev extends eqLogic {
     
-    public static $_modeDeveloppeur = 1;
+    public static $_modeDeveloppeur = 0;
     public static $_jsonDevEquipement = __DIR__ . "/../../../../plugins/scan_ip/data/json_dev/equipements.json";
     public static $_jsonDevCommentairesEquipement = __DIR__ . "/../../../../plugins/scan_ip/data/json_dev/commentMac.json";
     public static $_jsonDevMapping = __DIR__ . "/../../../../plugins/scan_ip/data/json_dev/mapping.json";
